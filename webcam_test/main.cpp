e8f6aabf5f8bf5231dd8cd1264fd01db750fae95
steven <sjcagle1@gmail.com>
2019-03-01 20:25:18 +0000

    Updated webcam for older webcams

--- webcam_test/main.cpp
+++ webcam_test/main.cpp
@@ -2,10 +2,18 @@
 //  Testing how to get input from a webcam using OpenCV
 
 #include <iostream>
-//#include <opencv2/opencv.hpp>  // Includes EVERYTHING
-#include <opencv2/videoio.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#if CV_MAJOR_VERISON < 4   // Tested using OpenCV 2.4.9 (through apt-get)
+    #include <opencv2/video/video.hpp>
+    #include <opencv2/imgproc/imgproc.hpp>
+    #include <opencv2/highgui/highgui.hpp>
+    #include <opencv2/core/core.hpp>
+#else                      // For newer versions
+//  #include <opencv2/opencv.hpp>  // Includes EVERYTHING
+    #include <opencv2/videoio.hpp>
+    #include <opencv2/imgproc.hpp>
+    #include <opencv2/highgui.hpp>
+    #include <opencv2/core.hpp>
+#endif
 
 using namespace cv;
 using namespace std;
@@ -14,7 +22,10 @@ int main(int, char**)
 {
     VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
+    {
+        cout << "Camera can't be opened, exiting!\n";
         return -1;
+    }
 
     try {
         Mat edges;
@@ -26,6 +37,8 @@ int main(int, char**)
         Mat small_frame;
         Size size = {orig_frame.cols/4, orig_frame.rows/4};
         resize(orig_frame, small_frame, size);
+        unsigned char* data = (unsigned char *)small_frame.data; 
+        cout << data << "\n";
         
         // Show Image
         imshow("test", small_frame);
@@ -35,6 +48,7 @@ int main(int, char**)
     catch (int e){
         cout << "An Exception occurred. Exception #" << e << "\n";
     }
+    cout << "Goodbye!\n";
 
     // Camera will be deinitialized automatically in VideoCapture destructor
     return 0;
