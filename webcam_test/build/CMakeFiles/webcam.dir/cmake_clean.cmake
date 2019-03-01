f178855d47639ade2d4ee2fdc295acc1167efed7
scagle <sjcagle1@gmail.com>
2019-02-28 19:18:30 -0800

    Made webcam test

---
+++ webcam_test/build/CMakeFiles/webcam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webcam.dir/main.cpp.o"
+  "webcam.pdb"
+  "webcam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webcam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ webcam_test/main.cpp
@@ -0,0 +1,38 @@
+// Webcam Tests
+//  Testing how to get input from a webcam using OpenCV
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**)
+{
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    try {
+        Mat edges;
+        namedWindow("test", 1);
+        // Get Image
+        Mat orig_frame;
+        cap >> orig_frame; // get a new frame from camera
+        // Get Small Image
+        Mat small_frame;
+        Size size = {orig_frame.cols/4, orig_frame.rows/4};
+        resize(orig_frame, small_frame, size);
+        
+        // Show Image
+        imshow("test", small_frame);
+        waitKey(0);
+
+    } 
+    catch (int e){
+        cout << "An Exception occurred. Exception #" << e << "\n";
+    }
+
+    // Camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
