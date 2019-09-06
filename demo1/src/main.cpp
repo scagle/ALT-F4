8a253d916eca76c1c32ca4a5fdccb571917afe0f
Steven Cagle <sjcagle1@gmail.com>
2019-09-05 20:14:26 -0700

    Cleaned up main.cpp

--- demo1/src/main.cpp
+++ demo1/src/main.cpp
@@ -38,121 +38,55 @@
     #include <opencv2/features2d.hpp>
 #endif
 
-
 using namespace cv;
 using namespace std;
 
+VideoCapture initializeVideo();
 void printStats(VideoCapture, Mat);
+int processInputs(Mat);
 
-const string capture_name = "capture.png"; // Name of yanked original image
+const string capture_name = "capture.png"; // 'yanked' photo's name in filesystem
+deque<Frame>            frames(FRAME_FIFO_SIZE); // FIFO queue to store frames in
 
 int main(int, char**)
 {
-    VideoCapture cap(2);    // open the camera located at /dev/videoX
-    if (!cap.isOpened())    // check if we succeeded
-    {
-        cout << "Camera can't be opened, exiting!\n";
-        return -1;
-    }
-
-    // Setup VideoCapture settings (if not supported)
-    cout << "Setting up Camera settings..." << "\r";
-    cout.flush(); // actually print out (output is normally buffered)
-    cap.set(CAP_PROP_FPS, 30)           ; // 30 seems to be maximum. (may hang program depending on cam)
-    cap.set(CAP_PROP_FRAME_WIDTH, 320)  ; // Lowest possible 4:3 aspect ratio
-    cap.set(CAP_PROP_FRAME_HEIGHT, 240) ; // Lowest possible 4:3 aspect ratio
-    cap.set(CAP_PROP_BUFFERSIZE, 1)     ; // Reduce internal frame buffer size (we have our own)
-    cout << "Camera Settings Successfully Initialized!" << "\n";
+    VideoCapture cap = initializeVideo();
     // Variable Declaration
     Mat orig, hsv;       // Original and hue/sat/val matrixes
-    namedWindow("original", WINDOW_AUTOSIZE); // Declare window to draw into
-    namedWindow("modified", WINDOW_AUTOSIZE); // Declare window to draw into
-    deque<Frame> frames(FRAME_FIFO_SIZE); // FIFO queue to store frames in
+    namedWindow("original", WINDOW_AUTOSIZE); // Window to show original images
+    namedWindow("modified", WINDOW_AUTOSIZE); // Window to show modified images
 
     try { 
         // Frame-Size Dependant Variables
         cap >> orig; // get a frame from camera, so we can grab size
         printStats(cap, orig);
-        int channels = orig.channels();
-        int cols = orig.cols;
-        int width = orig.cols;
-        int rows = orig.rows;
-        int height = orig.rows;
-        vector<unsigned char> values(width * height * channels);  // initialize 'values' to correct size
 
-        while (1)
+        int channels = orig.channels();
+        int cols     = orig.cols;
+        int width    = orig.cols;
+        int rows     = orig.rows;
+        int height   = orig.rows;
+        vector< unsigned char > frame_data(width * height * channels);  // initialize 'frame_data' to correct size
+
+        int cont = 1; // continue variable 
+        while (cont == 1)
         {
             //// Capture Video
             cap >> orig; // get a frame from camera
             //cvtColor(orig, hsv, COLOR_BGR2HSV); // convert to hsv
-            values.assign(orig.data, orig.data + orig.total() * channels); // copy opencv 'Mat' as single vector
+            frame_data.assign(orig.data, orig.data + orig.total() * channels); // copy opencv 'Mat' as single vector
 
             //// Manipulate FIFO queue
             frames.pop_back();
-            frames.push_front(Frame(values, width, height, channels));
+            frames.push_front(Frame(frame_data, width, height, channels));
             
             
-            Mat frame_mat = frames.back().getMat();
+            Mat frame_mat = frames.front().getMat();
                 
             imshow("modified", frame_mat);
-
-            ////// Perform statistics
-            //// Test
-            //vector<unsigned int> sums = {0, 0, 0};
-            //unsigned int num = 0;
-            //for (int i = 0; i < FRAME_FIFO_SIZE; i++)
-            //{
-            //    if (!frames[i].isInitialized())
-            //    {
-            //        cout << "frames[" << i << ":] is uninitialized!\n";
-            //        break;
-            //    }
-            //    vector<unsigned char> data = frames[i].getValues();
-            //    for (int row = 0; row < orig.rows; row++)
-            //    {
-            //        for (int column = 0; column < orig.cols; column++)
-            //        {
-            //            float b = data[(channels * orig.cols) * row + (column * channels) + 0];
-            //            float g = data[(channels * orig.cols) * row + (column * channels) + 1];
-            //            float r = data[(channels * orig.cols) * row + (column * channels) + 2];
-            //            sums[0] += b;
-            //            sums[1] += g;
-            //            sums[2] += r;
-            //            num += 1;
-            //        }
-            //    }
-            //    //cout << "frame[" << i << "] average bgr is: (" << sums[0] / num << ", " << sums[1] / num << ", " << sums[2] / num << ")\n";
-            //    sums = {0, 0, 0};
-            //    num = 0;
-            //}
-            //cout << "*****************************************************************\n";
-            
             imshow("original", orig);
 
-            // Delay, and Handle character inputs
-            unsigned char key = waitKey(1);
-            switch (key)
-            {
-                case 'y':  // (Yank) Capture a still of original frame, save it to file, and display it on another window
-                    {
-                        Mat captured_image; // Original, monochrome (grayscale), and hue/sat/val matrixes
-                        imwrite(capture_name, hsv);
-                        captured_image = imread(capture_name, 1);
-                        imshow("saved", hsv);
-                        break;
-                    }
-                case 's':  // Capture a screenshot image, and save it to screenshots/ directory   
-                    {
-                        int error = system("scrot -e 'mv $f ../screenshots/'"); // Capture screenshot
-                        if (error)
-                            cout << "ERROR: Screen Shot failed! (do you have the 'scrot' package installed?)" << "\n";
-                        else
-                            cout << "Screen Shot saved!" << "\n";
-                        break;
-                    }
-            }
-            if (key == 'q')
-                break;
+            cont = processInputs(frame_mat);
         }
     }
     catch (int e){
@@ -162,6 +96,25 @@ int main(int, char**)
 
     // Camera will be deinitialized automatically in VideoCapture destructor
     return 0;
+}
+
+VideoCapture initializeVideo()
+{
+    VideoCapture cap(2);    // open the camera located at /dev/videoX
+    if (!cap.isOpened())    // check if we succeeded
+    {
+        cout << "Camera can't be opened, exiting!\n";
+        return -1;
+    }
+    // Setup VideoCapture settings (if not supported)
+    cout << "Setting up Camera settings..." << "\r";
+    cout.flush(); // actually print out (output is normally buffered)
+    cap.set(CAP_PROP_FPS, 30)           ; // 30 seems to be maximum. (may hang program depending on cam)
+    cap.set(CAP_PROP_FRAME_WIDTH, 320)  ; // Lowest possible 4:3 aspect ratio
+    cap.set(CAP_PROP_FRAME_HEIGHT, 240) ; // Lowest possible 4:3 aspect ratio
+    cap.set(CAP_PROP_BUFFERSIZE, 1)     ; // Reduce internal frame buffer size (we have our own)
+    cout << "Camera Settings Successfully Initialized!" << "\n";
+    return cap;
 }
 
 void printStats(VideoCapture cap, Mat img)
@@ -184,3 +137,33 @@ void printStats(VideoCapture cap, Mat img)
     }
 }
 
+int processInputs(Mat yank_mat)
+{
+    // Delay, and Handle character inputs
+    unsigned char key = waitKey(1); 
+    switch (key)
+    {
+        case 'y':  // (Yank) Capture a still of original frame, save it to file, and display it on another window
+            {
+                Mat captured_image; // Original, monochrome (grayscale), and hue/sat/val matrixes
+                imwrite(capture_name, yank_mat);
+                captured_image = imread(capture_name, 1);
+                imshow("yanked", yank_mat);
+                break;
+            }
+        case 's':  // Capture a screenshot image, and save it to screenshots/ directory   
+            {
+                int error = system("scrot -e 'mv $f ../screenshots/'"); // Capture screenshot
+                if (error)
+                    cout << "ERROR: Screen Shot failed! (do you have the 'scrot' package installed?)" << "\n";
+                else
+                    cout << "Screen Shot saved!" << "\n";
+                break;
+            }
+    }
+    if (key == 'q')
+    {
+        return -1;
+    }
+    return 1;
+}
