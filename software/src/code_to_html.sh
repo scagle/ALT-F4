18475dd46fbf1b8c50a0e38635de2678c1764548
Steven Cagle <sjcagle1@gmail.com>
2019-12-20 02:57:20 -0800

    Created Source Code to HTML Bash Script

---
+++ software/src/code_to_html.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILES=`{ git ls-tree -r HEAD --name-only; git status | grep "new file" | awk '"'"'{ print $3 }'"'"'; } | grep -e "\.\(c\|cpp\)$" -e "\.\(h\|hpp\)$" -e "\.\(h\|c\)$" -e "\.\(h\|h\)$"| env LC_COLLATE=C sort`
+
+echo "$FILES"
+
+for FILE in $FILES
+do
+    # will ignore any file in subdirectories that don't exist in html ( which is a blessing and a curse )
+    #nvim "$FILE" -c "colorscheme github" -c "TOhtml" -c "%s/1em/7/g" -c "%s/eeeeee/ffffff/g" -c "%s/monospace/"'"'"Courier New"'"'"/g" -c "w! html/$FILE.html" -c "qa!" 
+done
